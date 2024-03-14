b5df7dff28843bba5872e50bc27c7a9aaa8c3de0
Janarthanan Kannan <tjana17@gmail.com>
2024-03-14 13:11:47 +0530

    Jana-portfolio website

---
+++ form_submit.php
@@ -0,0 +1,28 @@
+<?php
+echo "suss";
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['comments'])) {
+    // $fname = $_POST['fname'];
+    // $lname = $_POST['lname'];
+    // $email = $_POST['email'];
+    // $phone = $_POST['phone'];
+    // $comments = $_POST['comments'];
+
+    // $to = "jana@qpaymentz.com";
+    // $subject = "Contact form submission";
+    // $headers  = 'MIME-Version: 1.0' . "\r\n";
+    // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    // // Create email headers
+    // $headers .= 'From: '. $fname . '<' .$email. '>' ."\r\n".
+    // 'Reply-To: '.$email."\r\n" .
+    // 'X-Mailer: PHP/' . phpversion();
+    // $headers .= 'Cc: jana@qpaymentz.com' . "\r\n";
+    // echo $headers;
+    // $body = file_get_contents('mailer_templates/email_skeleton.html');
+    // $body = str_replace("{{fname}}", $fname, $body);
+//     echo $email;
+// } else {
+//          http_response_code(403);
+//         echo "Hubo un problema con tu mensaje por favor intenta nuevamente";
+//     }
+?>
